54b2a6c048811734e96735bcc1e5dd4ec1b68aab
mmsonido <mauromodica98@gmail.com>
2025-06-01 21:56:02 -0300

    chore: minor change

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,13 @@
 terraform {
   required_version = ">=1.5"
-
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
   }
-
   backend "gcs" {
-    bucket = "tf-state-bucket"
+    bucket = "tf-state-bucket-pdf2txt-demo"
     prefix = "pdf2txt"
   }
 }
